0c301edb049211d24428e8002c78df693e05a0a5
Bruno_Henrique <bruno.ofc777@gmail.com>
2022-02-21 18:58:22 -0300

    finalizandi calculadora

---
+++ script.js
@@ -0,0 +1,15 @@
+let valor;
+let resultado;
+
+function botao(num){
+	valor = document.calc.visor.value += num
+}
+
+function resetar(){
+	document.calc.visor.value = '' 
+}
+
+function calcular(){
+	resultado = eval(valor)
+	document.calc.visor.value = resultado.toLocaleString("pt-br")
+}
